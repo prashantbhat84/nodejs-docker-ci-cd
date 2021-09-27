1057abc3c7f58c0d1f500f7b82d9bf5f77e565fe
Prashant Bhat <prashantbhat84@gmail.com>
2021-09-27 11:58:04 +0530

    Github CI/CD workflow
    testing triggering

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/status', (req, res) => res.send({status: "I'm alive!"}));
+app.get('/status', (req, res) => res.send({status: "I'm working!"}));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
