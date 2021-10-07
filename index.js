48e3dc10c7cbc489d7f8d9f9ccf4519fb9cbc0a1
Prashant Bhat <prashantbhat84@gmail.com>
2021-10-07 15:29:20 +0530

    test file

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send({status: "Hey Prashant All  well !"}));
+app.get('/', (req, res) => res.send({status: "Hey Prashant All is  well !"}));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
